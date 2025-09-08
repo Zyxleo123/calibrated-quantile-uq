a10a59a17d8cf9c8f283a5734c78c4dde3ef65cf
Yixi Zhao <yixiz@andrew.cmu.edu>
2025-09-08 19:03:14 -0400

    added seed aggregation plot

--- test_plot_script.py
+++ test_plot_script.py
@@ -6,19 +6,19 @@ from plots.plot_metrics import plot_training_stats, compare_ece_sharpness, compa
 from plots.plot_utils import load_pickle
 
 def main():
-    pkls = glob.glob("results/*.pkl")
+    pkls = glob.glob("/home/scratch/yixiz/results/test_seeds/boston/test/*.pkl")
     for pkl in pkls:
         data = load_pickle(pkl)
         # outdir = "/home/scratch/yixiz/results"
-        outdir = "results"
+        outdir = "/home/scratch/yixiz/results/test_seeds/boston/test/"
         if os.path.isdir(os.path.join(outdir, pkl)):
             print("Directory already exists:", os.path.join(outdir, pkl))
             continue
         pkl, ext = os.path.splitext(os.path.basename(pkl))
         plot_training_stats(data, outpath=os.path.join(outdir, pkl + "/training_stats.png"))
-        compare_ece_sharpness(data, outpath=os.path.join(outdir, pkl + "/ece_sharpness.png"))
-        compare_scoring_rules(data, outpath=os.path.join(outdir, pkl + "/scoring_rules.png"))
-        calibration_plot(data, outpath=os.path.join(outdir, pkl + "/calibration_plot.png"))
+        # compare_ece_sharpness(data, outpath=os.path.join(outdir, pkl + "/ece_sharpness.png"))
+        # compare_scoring_rules(data, outpath=os.path.join(outdir, pkl + "/scoring_rules.png"))
+        # calibration_plot(data, outpath=os.path.join(outdir, pkl + "/calibration_plot.png"))
         plot_ece_sharpness(data, outpath=os.path.join(outdir, pkl + "/ece_sharpness.png"))
         print(f"Plot of {pkl} saved to {outdir}")
 
