5a2414e4f548fff8389982f36ebb9c194fe6f937
Yixi Zhao <yixiz@andrew.cmu.edu>
2025-09-18 12:47:22 -0400

    add new data

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -6,8 +6,48 @@ from argparse import Namespace
 
 PR_DIR = "/zfsauton/project/public/ysc/"
 
+def get_facebook_data(args):
+    train_data = np.loadtxt("{}/facebook_train.txt".format(args.data_dir))
+    test_data = np.loadtxt("{}/facebook_test.txt".format(args.data_dir))
+    x_tr = train_data[:, :-1]
+    y_tr = train_data[:, -1].reshape(-1, 1)
+    x_te = test_data[:, :-1]
+    y_te = test_data[:, -1].reshape(-1, 1)
+
+    x_al = np.concatenate([x_tr, x_te], axis=0)
+    y_al = np.concatenate([y_tr, y_te], axis=0)
+
+    x_tr, x_va, y_tr, y_va = train_test_split(
+        x_tr, y_tr, test_size=0.2, random_state=args.seed
+    )
+
+    s_tr_x = StandardScaler().fit(x_tr)
+    s_tr_y = StandardScaler().fit(y_tr)
+
+    x_tr = torch.Tensor(s_tr_x.transform(x_tr))
+    x_va = torch.Tensor(s_tr_x.transform(x_va))
+    x_te = torch.Tensor(s_tr_x.transform(x_te))
+
+    y_tr = torch.Tensor(s_tr_y.transform(y_tr))
+    y_va = torch.Tensor(s_tr_y.transform(y_va))
+    y_te = torch.Tensor(s_tr_y.transform(y_te))
+    y_al = torch.Tensor(s_tr_y.transform(y_al))
+
+    out_namespace = Namespace(
+        x_tr=x_tr,
+        x_va=x_va,
+        x_te=x_te,
+        y_tr=y_tr,
+        y_va=y_va,
+        y_te=y_te,
+        y_al=y_al,
+    )
+
+    return out_namespace
 
 def get_uci_data(args):
+    if args.data == 'facebook':
+        return get_facebook_data(args)
     data = np.loadtxt("{}/{}.txt".format(args.data_dir, args.data))
     x_al = data[:, :-1]
     y_al = data[:, -1].reshape(-1, 1)
