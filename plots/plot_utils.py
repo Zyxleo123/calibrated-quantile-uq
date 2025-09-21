a5e23d27a4d93ae07f754a37f9ad602b3f36e1fa
Yixi Zhao <yixiz@andrew.cmu.edu>
2025-09-21 18:19:46 -0400

    Add plotting constants

--- plots/plot_utils.py
+++ plots/plot_utils.py
@@ -1,5 +1,55 @@
 import pickle
 from typing import Any, Dict
+import os
+import matplotlib.cm as cm
+
+RESULTS_BASE_DIR = os.path.join(os.environ.get('SCRATCH', '.'), "results")
+BASELINE_NAMES = ['batch_qr', 'batch_cal', 'batch_int', 'maqr', 'calipso']
+HYPER_NAMES = ['nl-1_hs-32', 'nl-2_hs-64', 'nl-4_hs-128', 'nl-8_hs-256']
+SEEDS = ['_0', '_1', '_2', '_3', '_4']
+Y_METRICS = [
+    'te_sharp_score_controlled',
+    'te_variance_controlled',
+    'te_mpiw_controlled',
+    'te_crps_controlled',
+    'te_interval_controlled',
+    'te_check_controlled',
+    'te_bag_nll_controlled',
+]
+TITLE_METRICS = {
+    'te_sharp_score_controlled': 'Sharpness',
+    'te_variance_controlled': 'Variance',
+    'te_mpiw_controlled': 'MPIW',
+    'te_crps_controlled': 'CRPS',
+    'te_interval_controlled': 'Interval Score',
+    'te_check_controlled': 'Check Score',
+    'te_bag_nll_controlled': 'Bag NLL',
+    'te_va_ece_exceedance': 'ECE Exceedance'
+}
+TITLE_METHODS = {
+    'batch_qr': 'QR',
+    'batch_cal': 'Cali',
+    'batch_int': 'Interval',
+    'maqr': 'MAQR',
+    'calipso': 'Calipso'
+}
+PERFORMANCE_METRICS = ['IGD', 'IGD+', 'GD', 'GD+', 'HV']
+COLORS = cm.get_cmap('tab10', max(len(BASELINE_NAMES), len(HYPER_NAMES)))
+METHOD_COLORS = {method: COLORS(i) for i, method in enumerate(BASELINE_NAMES)}
+HYPER_COLORS = {hyper: COLORS(i) for i, hyper in enumerate(HYPER_NAMES)}
+METHOD_MARKERS = {
+    'batch_qr': 'o',  # Circle
+    'batch_cal': 's', # Square
+    'batch_int': '^', # Triangle up
+    'maqr': 'D',      # Diamond
+    'calipso': 'X',   # X
+}
+HYPER_MARKERS = {
+    'nl-1_hs-32': 'o',   # Circle
+    'nl-2_hs-64': 's',   # Square
+    'nl-4_hs-128': '^',  # Triangle up
+    'nl-8_hs-256': 'D',  # Diamond
+}
 
 def load_pickle(path: str) -> Dict[str, Any]:
     """Load a pickle that contains the save_var_names dict data."""
